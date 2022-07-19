1358fb86f3ff6fa6ae99f8a0343d3b38c3974c97
JAE YEON KIM <65452447+jaethewiederholen@users.noreply.github.com>
2022-07-19 16:03:26 +0900

    token contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,26 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      accounts: {
+        count: 20,
+      },
+      chainId: 1337,
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
